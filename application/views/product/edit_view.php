df0285485986b6087b0f0901b2d0408a98ebe428
unamedos <unamedos@gmail.com>
2020-08-06 17:16:34 -0400

    user_guide

--- application/views/product/edit_view.php
+++ application/views/product/edit_view.php
@@ -2,14 +2,14 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>
-        Editar Producto
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Forms</a></li>
-        <li class="active">General Elements</li>
-      </ol>
+        <h1>
+            Editar Producto
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#">Forms</a></li>
+            <li class="active">General Elements</li>
+        </ol>
     </section>
 
     <!-- Main content -->
@@ -23,22 +23,41 @@
                     <form action="<?php echo base_url(); ?>product/update" method="POST" class="form-horizontal">
                         <div class="box-body">
 
-                            <input type="hidden" name="id" value="<?php echo $row->id; ?>">
-
                             <div class="form-group">
                                 <label for="inputEmail3" class="col-sm-2 control-label">Categorias</label>
                                 <div class="col-sm-10">
-                    
+                                    <select name="fk_categoria" class="form-control">
+                                        <option value="">Seleccione una opcion</option>
+                                        <?php
+                                        foreach ($listadoCategorias as $row) {
+                                            echo "<option value=" . $row->id . ">" . $row->nombre . "</option>";
+                                        }
+                                        ?>
+                                    </select>
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="inputEmail3" class="col-sm-2 control-label">Nombre de categoria </label>
+                                <label for="inputEmail3" class="col-sm-2 control-label">Nombre de Producto</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" name="nombre" value="<?php echo $row->nombre; ?>" placeholder="Ingrese nombre producto">
                                 </div>
                             </div>
-                            
+
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">Precio Unitario</label>
+                                <div class="col-sm-10">
+                                    <input type="number" class="form-control" name="precio_unitario" value=" <?php echo $row->precio_unitario ?>" placeholder="Ingrese Precio Unitario">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">Cantidad</label>
+                                <div class="col-sm-10">
+                                    <input type="number" class="form-control" name="cantidad" value="<?php echo $row->cantidad ?>" placeholder="Ingrese nombre Cantidad">
+                                </div>
+                            </div>
+
                         </div>
                         <!-- /.box-body -->
                         <div class="box-footer">
@@ -54,5 +73,5 @@
         <!-- /.row -->
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+<!-- /.content-wrapper -->
